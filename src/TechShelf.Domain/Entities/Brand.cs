eac3755bbbb97d8dfeadc206e55c685b39153ff3
Danilo Chaku <danilochaku@gmail.com>
2024-12-06 21:21:13 +0200

    feat(entities): add Category, Brand and Product entities

---
+++ src/TechShelf.Domain/Entities/Brand.cs
@@ -0,0 +1,8 @@
+﻿using TechShelf.Domain.Common;
+
+namespace TechShelf.Domain.Entities;
+
+public class Brand : Entity<int>
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ src/TechShelf.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿using TechShelf.Domain.Common;
+
+namespace TechShelf.Domain.Entities;
+
+public class Category : Entity<int>
+{
+    public string Name { get; set; } = string.Empty;
+}
