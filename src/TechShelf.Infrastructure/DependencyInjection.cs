705ac7fab3d8ae9101505513cb2705a812717f4e
Danilo Chaku <danilochaku@gmail.com>
2024-12-08 13:07:50 +0200

    feat(infrastructure): configure database context with PostgreSQL

---
+++ src/TechShelf.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechShelf.Infrastructure.Data;
+
+namespace TechShelf.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
+
+        return services;
+    }
+}
