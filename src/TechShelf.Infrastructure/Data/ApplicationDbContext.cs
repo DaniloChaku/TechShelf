d815eb58f346c2a613b02d73c61e91ebf9b65ccc
Danilo Chaku <danilochaku@gmail.com>
2024-12-08 09:45:40 +0200

    feat(infrastructure): add ApplicationDbContext for Ef Core

---
+++ src/TechShelf.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechShelf.Domain.Entities;
+
+namespace TechShelf.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+#pragma warning disable CS8618
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+}
