8888c7fff2f928d517871041404af3285a4e60cb
Danilo Chaku <danilochaku@gmail.com>
2024-12-11 11:07:53 +0200

    fix(dto): add Id property to ProductDto

--- src/TechShelf.Application/Features/Products/Queries/Shared/ProductDto.cs
+++ src/TechShelf.Application/Features/Products/Queries/Shared/ProductDto.cs
@@ -5,6 +5,7 @@ namespace TechShelf.Application.Features.Products.Queries.Shared;
 
 public class ProductDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int CategoryId { get; set; }
