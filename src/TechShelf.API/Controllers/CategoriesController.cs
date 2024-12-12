358544cecb3f657a0afb14d814b9b8309ad72e64
Danilo Chaku <danilochaku@gmail.com>
2024-12-12 13:40:15 +0200

    feat(categories): implement category listing endpoint in CategoriesController

---
+++ src/TechShelf.API/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TechShelf.Application.Features.Categories.Queries.GetAllCategories;
+using TechShelf.Application.Features.Categories.Queries.Shared;
+
+namespace TechShelf.API.Controllers;
+
+public class CategoriesController : BaseApiController
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CategoryDto>>> GetAll()
+    {
+        var query = new GetAllCategoriesQuery();
+        var categories = await _mediator.Send(query);
+
+        return categories;
+    }
+}
---
+++ tests/TechShelf.UnitTests/Api/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using TechShelf.API.Controllers;
+using TechShelf.Application.Features.Categories.Queries.GetAllCategories;
+using TechShelf.Application.Features.Categories.Queries.Shared;
+
+namespace TechShelf.UnitTests.Api.Controllers;
+
+public class CategoriesControllerTests
+{
+    private readonly Fixture _fixture;
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly CategoriesController _controller;
+
+    public CategoriesControllerTests()
+    {
+        _fixture = new Fixture();
+        _mediatorMock = new Mock<IMediator>();
+        _controller = new CategoriesController(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsListOfCategoryDtos_WhenCategoriesExist()
+    {
+        // Arrange
+        var expectedCategories = _fixture.CreateMany<CategoryDto>(5).ToList();
+
+        _mediatorMock
+            .Setup(m => m.Send(It.IsAny<GetAllCategoriesQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedCategories);
+
+        // Act
+        var result = await _controller.GetAll();
+
+        // Assert
+        result.Value.Should().BeEquivalentTo(expectedCategories);
+
+        _mediatorMock.Verify(m => m.Send(It.IsAny<GetAllCategoriesQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
