10941a872ec3a6002f27c5400cccca41dc4eeb33
Danilo Chaku <danilochaku@gmail.com>
2024-12-12 12:05:47 +0200

    feat(brands): implement brand listing endpoint in BrandsController

---
+++ src/TechShelf.API/Controllers/BrandsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TechShelf.Application.Features.Brands.Queries.GetAllBrands;
+using TechShelf.Application.Features.Brands.Queries.Shared;
+
+namespace TechShelf.API.Controllers;
+
+public class BrandsController : BaseApiController
+{
+    private readonly IMediator _mediator;
+
+    public BrandsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BrandDto>>> GetAll()
+    {
+        var query = new GetAllBrandsQuery();
+        var brands = await _mediator.Send(query);
+
+        return brands;
+    }
+}
---
+++ tests/TechShelf.UnitTests/Api/Controllers/BrandsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TechShelf.API.Controllers;
+using TechShelf.Application.Features.Brands.Queries.GetAllBrands;
+using TechShelf.Application.Features.Brands.Queries.Shared;
+
+namespace TechShelf.UnitTests.Api.Controllers;
+
+public class BrandsControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly BrandsController _controller;
+
+    public BrandsControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _controller = new BrandsController(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnsListOfBrandDtos_WhenBrandsExist()
+    {
+        // Arrange
+        var expectedBrands = new List<BrandDto>
+        {
+            new (1, "Brand1"),
+            new (2, "Brand2")
+        };
+
+        _mediatorMock
+            .Setup(m => m.Send(It.IsAny<GetAllBrandsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedBrands);
+
+        // Act
+        var result = await _controller.GetAll();
+
+        // Assert
+        result.Value.Should().BeEquivalentTo(expectedBrands);
+
+        _mediatorMock.Verify(m => m.Send(It.IsAny<GetAllBrandsQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
