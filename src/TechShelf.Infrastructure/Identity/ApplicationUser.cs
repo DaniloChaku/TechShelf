61e6c1523c85de0219e8743d06dc6f0f170c064f
Danilo Chaku <danilochaku@gmail.com>
2024-12-17 19:16:22 +0200

    feat(identity): add length constraints for user first and last names

--- src/TechShelf.Infrastructure/Identity/ApplicationUser.cs
+++ src/TechShelf.Infrastructure/Identity/ApplicationUser.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace TechShelf.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
+    [StringLength(50)]
     public string FirstName { get; set; } = string.Empty;
+    [StringLength(50)]
     public string LastName { get; set; } = string.Empty;
 }
---
+++ src/TechShelf.Infrastructure/Identity/Migrations/20241217171504_SetFirstAndLastNameLength.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechShelf.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetFirstAndLastNameLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
