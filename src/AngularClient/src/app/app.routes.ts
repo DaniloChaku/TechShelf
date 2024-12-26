68b69f3c1a9116b87ada4e44fd20ce0eee461661
Danilo Chaku <danilochaku@gmail.com>
2024-12-26 09:49:41 +0200

    feat(products): add navigation to individual product pages

--- src/AngularClient/src/app/app.routes.ts
+++ src/AngularClient/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './features/products/products.component';
+import { ProductDetailsComponent } from './features/products/product-details/product-details.component';
 
 export const routes: Routes = [
   { path: '', component: ProductsComponent },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent,
+  },
 ];
--- src/AngularClient/src/app/features/products/product-card/product-card.component.ts
+++ src/AngularClient/src/app/features/products/product-card/product-card.component.ts
@@ -3,11 +3,17 @@ import { Product } from '../../../core/models/product';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, CurrencyPipe],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    CurrencyPipe,
+    RouterLink,
+  ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
