d19af488c8083dbecfea27613e167b8232aa77b5
Danilo Chaku <danilochaku@gmail.com>
2024-12-26 20:12:04 +0200

    feat(products): implement product details page

--- src/AngularClient/src/app/features/products/product-details/product-details.component.ts
+++ src/AngularClient/src/app/features/products/product-details/product-details.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../../../core/services/product/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../core/models/product';
+import { CurrencyPipe, Location } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatDivider,
+  ],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  private productService = inject(ProductService);
+  private activatedRoute = inject(ActivatedRoute);
+  private location = inject(Location);
+  product?: Product;
 
+  ngOnInit(): void {
+    var id =
+      this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.productService.getProductById(+id).subscribe({
+        next: (product) => (this.product = product),
+      });
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
