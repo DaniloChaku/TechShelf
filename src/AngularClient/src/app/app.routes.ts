b694b47613a7d4953886bb7c46b4afe9a9e22cae
Danilo Chaku <danilochaku@gmail.com>
2024-12-27 12:17:29 +0200

    feat(homepage): add homepage

--- src/AngularClient/src/app/app.routes.ts
+++ src/AngularClient/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './features/products/products.component';
 import { ProductDetailsComponent } from './features/products/product-details/product-details.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: ProductsComponent },
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: ProductsComponent },
   {
-    path: 'products/:id',
+    path: 'catalog/:id',
     component: ProductDetailsComponent,
   },
 ];
