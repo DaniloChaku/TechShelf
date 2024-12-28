09e7d5f4049d03a38bc281539541d8cedd1fc49d
Danilo Chaku <danilochaku@gmail.com>
2024-12-28 17:19:38 +0200

    feat(loading): add a loading bar under the header

--- src/AngularClient/src/app/layout/header/header.component.ts
+++ src/AngularClient/src/app/layout/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
+import {
+  Component,
+  inject,
+  ViewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import {
   RouterLink,
   RouterLinkActive,
 } from '@angular/router';
+import { LoadingService } from '../../core/services/loading.service';
 
 @Component({
   selector: 'app-header',
@@ -16,11 +22,13 @@ import {
     MatButtonModule,
     RouterLinkActive,
     MatSidenavModule,
+    MatProgressBarModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  loadingService = inject(LoadingService);
   isMenuOpen = false;
 
   toggleMenu() {
