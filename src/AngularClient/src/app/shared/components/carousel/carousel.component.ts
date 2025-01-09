f6d9cec479f210beb5d4951141de99d1c2772106
Danilo Chaku <danilochaku@gmail.com>
2025-01-09 19:00:19 +0200

    style(carousel): enhance slide transition arrow appearance

--- src/AngularClient/src/app/shared/components/carousel/carousel.component.ts
+++ src/AngularClient/src/app/shared/components/carousel/carousel.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faCircleArrowRight,
+  faCircleArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [FaIconComponent],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css',
 })
 export class CarouselComponent {
   @Input() items: string[] = [];
   currentSlide = 0;
+  faArrowRight = faCircleArrowRight;
+  faArrowLeft = faCircleArrowLeft;
 
   prev() {
     this.currentSlide =
