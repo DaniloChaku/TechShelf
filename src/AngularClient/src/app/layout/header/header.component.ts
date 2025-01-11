c85e315ce2578351e5a535eefe81aee84f553c61
Danilo Chaku <danilochaku@gmail.com>
2025-01-11 12:59:52 +0200

    feat(header): display shopping cart item count in header

--- src/AngularClient/src/app/layout/header/header.component.ts
+++ src/AngularClient/src/app/layout/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatBadgeModule } from '@angular/material/badge';
 import {
   RouterLink,
   RouterLinkActive,
@@ -10,6 +11,7 @@ import {
 import { LoadingService } from '../../core/services/loading/loading.service';
 import { UserService } from '../../core/services/user/user.service';
 import { MatMenuModule } from '@angular/material/menu';
+import { ShoppingCartService } from '../../core/services/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
@@ -22,6 +24,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSidenavModule,
     MatProgressBarModule,
     MatMenuModule,
+    MatBadgeModule,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
@@ -29,7 +32,14 @@ import { MatMenuModule } from '@angular/material/menu';
 export class HeaderComponent {
   loadingService = inject(LoadingService);
   userService = inject(UserService);
+  shoppingCartService = inject(ShoppingCartService);
   isMenuOpen = false;
+  cartItemsCount = computed(() =>
+    this.shoppingCartService.totalItems()
+  );
+  cartCountHidden = computed(
+    () => this.shoppingCartService.totalItems() == 0
+  );
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
