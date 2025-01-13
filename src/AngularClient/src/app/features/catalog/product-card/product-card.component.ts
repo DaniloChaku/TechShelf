4967de1ec32fd78a8a594c0d5901513a49a1b356
Danilo Chaku <danilochaku@gmail.com>
2025-01-13 12:51:06 +0200

    feat(products): enhance product card with add to cart button and dynamic state indication

--- src/AngularClient/src/app/features/catalog/product-card/product-card.component.ts
+++ src/AngularClient/src/app/features/catalog/product-card/product-card.component.ts
@@ -1,9 +1,20 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
 import { Product } from '../../../core/models/product';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { ShoppingCartService } from '../../../core/services/shopping-cart/shopping-cart.service';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faCartPlus,
+  faCheck,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-card',
@@ -13,10 +24,30 @@ import { RouterLink } from '@angular/router';
     MatIconModule,
     CurrencyPipe,
     RouterLink,
+    FaIconComponent,
   ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
+  shoppingCartService = inject(ShoppingCartService);
   product = input.required<Product>();
+  isInCart = computed<boolean>(() => {
+    return (
+      this.shoppingCartService
+        .cart()
+        .findIndex(
+          (p) => p.productId == this.product().id
+        ) > -1
+    );
+  });
+  faCartPlus = faCartPlus;
+  faCheck = faCheck;
+
+  addToCart() {
+    this.shoppingCartService.updateItem(
+      this.product().id,
+      1
+    );
+  }
 }
