b00a704d9cb150bcbf2085c9dae95ffac0b33eb9
Danilo Chaku <danilochaku@gmail.com>
2025-01-20 11:47:10 +0200

    feat(orders): add OrderById specification

---
+++ src/TechShelf.Domain/Specifications/Orders/OrderByIdSpec.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.Specification;
+using TechShelf.Domain.Entities.OrderAggregate;
+
+namespace TechShelf.Domain.Specifications.Orders;
+
+public class OrderByIdSpec : SingleResultSpecification<Order>
+{
+    public OrderByIdSpec(Guid orderId)
+    {
+        Query.Where(o => o.Id == orderId)
+            .Include(o => o.OrderItems)
+            .ThenInclude(i => i.ProductOrdered)
+            .Include(o => o.Address)
+            .Include(o => o.History);
+    }
+}
---
+++ tests/TechShelf.UnitTests/Domain/Specifications/OrderByIdSpecTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using TechShelf.Domain.Entities;
+using TechShelf.Domain.Entities.OrderAggregate;
+using TechShelf.Domain.Specifications.Orders;
+using TechShelf.Domain.Specifications.Products;
+
+namespace TechShelf.UnitTests.Domain.Specifications;
+
+public class OrderByIdSpecTests
+{
+    private readonly Fixture _fixture;
+
+    public OrderByIdSpecTests()
+    {
+        _fixture = new Fixture();
+
+        _fixture.Customize<Address>(composer => composer
+            .FromFactory(() => new Address(
+                country: Address.AllowedCountries[0],
+                _fixture.Create<string>(),
+                _fixture.Create<string>(),
+                _fixture.Create<string>(), 
+                _fixture.Create<string>(),
+                _fixture.Create<string>()
+            )));
+    }
+
+    [Fact]
+    public void MatchesOrderById()
+    {
+        // Arrange
+        var order = _fixture.Create<Order>();
+
+        var spec = new OrderByIdSpec(order.Id);
+
+        // Act
+        var result = spec.IsSatisfiedBy(order);
+
+        // Assert
+        result.Should().BeTrue();
+        spec.IncludeExpressions.Should().HaveCount(4);
+    }
+
+    [Fact]
+    public void MatchesNoProductById_WhenIdNotPresent()
+    {
+        // Arrange
+        var order = _fixture.Create<Order>();
+
+        var spec = new OrderByIdSpec(Guid.NewGuid());
+
+        // Act
+        var result = spec.IsSatisfiedBy(order);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
