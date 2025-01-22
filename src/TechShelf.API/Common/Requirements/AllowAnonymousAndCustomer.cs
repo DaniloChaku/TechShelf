27fc32a1dcfa69f31016e3f95f4e6f85d8fedb2b
Danilo Chaku <danilochaku@gmail.com>
2025-01-22 15:42:17 +0200

    feat: add AllowAnonymousAndCustomer requirement

---
+++ src/TechShelf.API/Common/Requirements/AllowAnonymousAndCustomer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TechShelf.Domain.Common;
+
+namespace TechShelf.API.Common.Requirements;
+
+public class AllowAnonymousAndCustomer : IAuthorizationHandler, IAuthorizationRequirement
+{
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (!context.User.Identity!.IsAuthenticated ||
+            context.User.IsInRole(UserRoles.Customer))
+        {
+            context.Succeed(this);
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/TechShelf.UnitTests/Api/Common/Requirements/AllowAnonymousAndCustomerTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using TechShelf.API.Common.Requirements;
+using TechShelf.Domain.Common;
+
+namespace TechShelf.UnitTests.Api.Common.Requirements;
+
+public class AllowAnonymousAndCustomerTests
+{
+    private readonly AllowAnonymousAndCustomer _handler;
+
+    public AllowAnonymousAndCustomerTests()
+    {
+        _handler = new AllowAnonymousAndCustomer();
+    }
+
+    [Fact]
+    public async Task HandleAsync_Succeeds_WhenUserIsNotAuthenticated()
+    {
+        // Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity());
+        var context = new AuthorizationHandlerContext(
+            [_handler],
+            user,
+            null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.True(context.HasSucceeded);
+    }
+
+    [Fact]
+    public async Task HandleAsync_Succeeds_WhenUserIsAuthenticatedAndInCustomerRole()
+    {
+        // Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity(
+        [
+                new Claim(ClaimTypes.Role, UserRoles.Customer)
+            ], "mock"));
+        var context = new AuthorizationHandlerContext(
+            [_handler],
+            user,
+            null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.True(context.HasSucceeded);
+    }
+
+    [Fact]
+    public async Task HandleAsync_DoesNotSucceed_WhenUserIsAuthenticatedAndNotInCustomerRole()
+    {
+        // Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity(
+        [
+                new Claim(ClaimTypes.Role, UserRoles.AdminSupport)
+            ], "mock"));
+        var context = new AuthorizationHandlerContext(
+            [_handler],
+            user,
+            null);
+
+        // Act
+        await _handler.HandleAsync(context);
+
+        // Assert
+        Assert.False(context.HasSucceeded);
+    }
+}
