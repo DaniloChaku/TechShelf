c66557435b3d16be72c0d772b91e53477c63ba34
Danilo Chaku <danilochaku@gmail.com>
2025-02-14 11:21:01 +0200

    feat(email): add IEmailService interface

---
+++ src/TechShelf.Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace TechShelf.Application.Interfaces.Services;
+
+public interface IEmailService
+{
+    Task SendPlainTextEmailAsync(
+        string toEmail,
+        string subject,
+        string message,
+        CancellationToken token = default);
+}
