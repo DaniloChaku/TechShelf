2fab371c5452536811f76ae01c2dbb9bc79efdd7
Danilo Chaku <danilochaku@gmail.com>
2025-03-08 08:13:23 +0200

    chore: remove unnecessary running profiles and set api port to 7000

--- src/AngularClient/src/environments/environment.development.ts
+++ src/AngularClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7281/api/',
+  apiUrl: 'https://localhost:7000/api/',
   stripeKey:
     'pk_test_51QjhtHAW8ObzjjuPYLAWq2MTnSQVqakQjgxAQEfaN41rUlwZ06vergSaYy6bnPy3KgEGHP9qhSn72OBTYBlixjBZ00AIgg7fTX',
 };
