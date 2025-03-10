d9b02f50bc7bf70b066889081f5fd12a62a98387
Danilo Chaku <danilochaku@gmail.com>
2025-03-10 10:48:21 +0200

    feat(client/users): add full name update form

---
+++ src/AngularClient/src/app/core/models/account/update-full-name-request.ts
@@ -0,0 +1,3 @@
+export interface UpdateFullNameRequest {
+  fullName: string;
+}
--- src/AngularClient/src/app/features/account/profile/profile.component.ts
+++ src/AngularClient/src/app/features/account/profile/profile.component.ts
@@ -1,10 +1,26 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { UserService } from '../../../core/services/user/user.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ApiError } from '../../../core/models/shared/api-error';
 
 @Component({
   selector: 'app-profile',
@@ -14,11 +30,66 @@ import { NotFoundComponent } from '../../../shared/components/not-found/not-foun
     MatIconModule,
     MatChipsModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
     NotFoundComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
   userService = inject(UserService);
+  private readonly snackBar = inject(MatSnackBar);
+  private readonly fb = inject(FormBuilder);
+  nameForm = this.fb.group({
+    fullName: [
+      '',
+      [Validators.required, Validators.maxLength(100)],
+    ],
+  });
+  isSubmitting = signal(false);
+
+  updateFirstName() {
+    if (this.nameForm.valid) {
+      this.isSubmitting.set(true);
+      const fullName =
+        this.nameForm.controls.fullName.value;
+
+      if (!fullName) return;
+
+      this.userService
+        .updateFullName({ fullName })
+        .subscribe({
+          next: () => {
+            this.snackBar.open(
+              'Full name updated successfully',
+              'Close',
+              {
+                duration: 3000,
+              }
+            );
+            this.userService.loadCurrentUser().subscribe();
+            this.nameForm.reset(
+              { fullName: '' },
+              { emitEvent: false }
+            );
+            this.isSubmitting.set(false);
+          },
+          error: (error) => {
+            const apiError = error.error as ApiError;
+            console.log(apiError);
+            const errorMessage = apiError.errors
+              ? apiError.errors['FullName'][0]
+              : 'Error updating first name';
+
+            this.snackBar.open(errorMessage, 'Close', {
+              duration: 3000,
+            });
+            this.isSubmitting.set(false);
+          },
+        });
+    }
+  }
 }
